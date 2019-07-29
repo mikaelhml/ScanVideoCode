e64227d4ae99f208565ec50744d7efb1651d366d
mikaelhml <mikaelhml@hotmail.com>
2019-07-29 19:53:46 -0300

    Criação de QRCode a partir de uma String

--- app/src/main/java/br/iesb/scanvideocode/MainActivity.kt
+++ app/src/main/java/br/iesb/scanvideocode/MainActivity.kt
@@ -1,11 +1,17 @@
 package br.iesb.scanvideocode
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
 import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.qrcode.QRCodeWriter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,24 +26,49 @@ class MainActivity : AppCompatActivity() {
         btnScanQRCode.setOnClickListener {
             iniciaScan()
         }
+        btnCriarQRCode.setOnClickListener {
+            criarQRCode()
+        }
     }
-    private fun iniciaScan(){
+
+    private fun criarQRCode() {
+        val writer = QRCodeWriter()
+        img_QRCode as ImageView
+        try {
+            val bitMatrix = writer.encode("teste", BarcodeFormat.QR_CODE, 512, 512)
+            val width = 512
+            val height = 512
+            val bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+            for (x in 0 until width) {
+                for (y in 0 until height) {
+                    if (!bitMatrix.get(x, y))
+                        bmp.setPixel(x, y, Color.WHITE)
+                    else
+                        bmp.setPixel(x, y, Color.BLACK)
+                }
+            }
+            img_QRCode.setImageBitmap(bmp)
+        } catch (e: WriterException) {
+            //Log.e("QR ERROR", ""+e);
+        }
+
+    }
+
+    private fun iniciaScan() {
         IntentIntegrator(this).initiateScan()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data)
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
 
-        if(result!=null){
-            if(result.contents == null){
-                Toast.makeText(this, "Está vazio",Toast.LENGTH_LONG).show()
-            }
-            else{
-                Toast.makeText(this,result.contents.toString(),Toast.LENGTH_LONG).show()
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Está vazio", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, result.contents.toString(), Toast.LENGTH_LONG).show()
             }
 
-        }
-        else{
+        } else {
 
         }
         super.onActivityResult(requestCode, resultCode, data)
